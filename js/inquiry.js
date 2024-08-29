b306c8f0a39c9bc68ea93a97c2aa4caef598d12f
karvinmc <karvinmciputra@gmail.com>
2024-08-29 19:40:06 +0700

    inquiry fix :)

--- js/inquiry.js
+++ js/inquiry.js
@@ -1,13 +1,19 @@
 function sendEmail() {
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "username",
-        Password : "password",
-        To : 'them@website.com',
-        From : "you@isp.com",
-        Subject : "This is the subject",
-        Body : "And this is the body"
-    }).then(
-      message => alert(message)
-    );
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "username",
+    Password: "password",
+    To: "teddywirawan78@yahoo.com",
+    From: document.getElementById("email").value, // Fetches email from form input
+    Subject: "Inquiry from " + document.getElementById("name").value,
+    Body: `
+      Name: ${document.getElementById("name").value}
+      <br>Email: ${document.getElementById("email").value}
+      <br>Whatsapp Number: ${document.getElementById("waNumber").value}
+      <br>Event: ${document.getElementById("event").value}
+      <br>Special Request: ${document.getElementById("specialReq").value}
+    `
+  })
+    .then((message) => alert("Mail sent successfully: " + message))
+    .catch((error) => alert("Error sending mail: " + error))
 }
